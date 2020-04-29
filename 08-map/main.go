d1a19bab93dee7dc518699a2a527811dc0b4f3ab
zakiydani <zakiydani87@gmail.com>
2020-04-29 22:04:50 +0700

    Update

---
+++ 08-map/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+type Product struct {
+	 Name string
+	 Price int
+	 Stock int
+}
+
+func main() {
+	var Item = make(map[int]Product)
+
+	Item[] = Product{Name:"Masker", Harga:10000, Stock:9}
+	Item[] = Product{Name:"Sarung Tangan", Harga:20000, Stock:5}
+	Item[] = Product{Name:"Hand Sanitizer", Harga:25000, Stock:8}
+	Item[] = Product{Name:"Sabun", Harga:3000, Stock:30}
+	Item[] = Product{Name:"Shampoo", Harga:17000, Stock:50}
+
+}
